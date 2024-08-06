fce6395d5ed98575f7df24a5e582bd0a31d7cb32
Yellow Shine <sammy.huang@zilliz.com>
2024-08-06 10:22:59 +0800

    bump to 0.6.1
    Signed-off-by: Yellow Shine <sammy.huang@zilliz.com>

--- jenkins-shared-library/vars/tekton.groovy
+++ jenkins-shared-library/vars/tekton.groovy
@@ -38,7 +38,7 @@ spec:
     resolver: bundles
     params:
     - name: bundle
-      value: harbor.milvus.io/milvusdb/tekton/build-milvus:0.6
+      value: harbor.milvus.io/milvusdb/tekton/build-milvus:0.6.1
     - name: name
       value: conanfile-build
     - name: kind
